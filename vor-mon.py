abbb9a9a96e82f4c32d097f2a4a6763feb2c9291
Nick Pappin <npappin@treze.lan>
2020-11-11 11:30:24 -0800

    Ran black to format the codebase.

--- vor-mon.py
+++ vor-mon.py
@@ -37,37 +37,58 @@ VERSION
 import sys, os, traceback, argparse
 import time
 
+
 def version():
-  print('vor-mon.py -- 0.0.1')
-  return 0
+    print("vor-mon.py -- 0.0.1")
+    return 0
 
-def main (options):
-  # TODO: Do something more interesting here...
-  print('Hello world!')
-  return 0
 
-if __name__ == '__main__':
-  try:
-    start_time = time.time()
-    helpDescription = "TODO: Write a description"
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description=helpDescription)
-    parser.add_argument ('-v', '--verbose', action='store_true', default=False, help='Change to verbose output')
-    parser.add_argument ('--version', action='store_true', default=False, help='Print version')
-    options = parser.parse_args()
-    if len(sys.argv) < 1: parser.error ('missing argument')
-    if options.verbose: print(time.asctime())
-    if options.version == False: funcReturn=main(options)
-    if options.version == True: funcReturn=version()
-    if options.verbose: print(time.asctime())
-    if options.verbose: print('TOTAL TIME IN MINUTES:')
-    if options.verbose: print((time.time() - start_time) / 60.0)
-    sys.exit(funcReturn)
-  except KeyboardInterrupt as e: # Ctrl-C
-    raise e
-  except SystemExit as e: # sys.exit()
-    raise e
-  except Exception as e:
-    print('ERROR, UNEXPECTED EXCEPTION')
-    print(str(e))
-    traceback.print_exc()
-    os._exit(1)
+def main(options):
+    # TODO: Do something more interesting here...
+    print("Hello world!")
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        start_time = time.time()
+        helpDescription = "TODO: Write a description"
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description=helpDescription,
+        )
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            default=False,
+            help="Change to verbose output",
+        )
+        parser.add_argument(
+            "--version", action="store_true", default=False, help="Print version"
+        )
+        options = parser.parse_args()
+        if len(sys.argv) < 1:
+            parser.error("missing argument")
+        if options.verbose:
+            print(time.asctime())
+        if options.version == False:
+            funcReturn = main(options)
+        if options.version == True:
+            funcReturn = version()
+        if options.verbose:
+            print(time.asctime())
+        if options.verbose:
+            print("TOTAL TIME IN MINUTES:")
+        if options.verbose:
+            print((time.time() - start_time) / 60.0)
+        sys.exit(funcReturn)
+    except KeyboardInterrupt as e:  # Ctrl-C
+        raise e
+    except SystemExit as e:  # sys.exit()
+        raise e
+    except Exception as e:
+        print("ERROR, UNEXPECTED EXCEPTION")
+        print(str(e))
+        traceback.print_exc()
+        os._exit(1)
